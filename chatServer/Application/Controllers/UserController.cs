9fe57a6b7e9a4aaefd452743e9ba8433f1e23d0a
Bruno <71056568+brunoo-p@users.noreply.github.com>
2021-05-09 18:53:42 -0300

    Heroku Chat

--- chatServer/Application/Controllers/UserController.cs
+++ chatServer/Application/Controllers/UserController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Chat.Application.Controllers
 {   
+    [EnableCors("Client")]
     [ApiController]
     [Route("[controller]")]
     public class UserController : Controller
